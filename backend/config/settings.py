6f5bd4025571a63ce6294d5067fd4d11f38ecb98
Priyanka Kumari <priyanka@Priyankas-MacBook-Air.local>
2026-05-29 07:50:05 +0530

    update settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -79,7 +79,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://data-ingestion-dashboard-1.onrender.com",
+]
 
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
